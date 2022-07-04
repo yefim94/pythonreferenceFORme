6759e4c3ab94faeaea4605b6988b5b3e387476d9
Yefim Blokh <yefim94@yahoo.com>
2022-07-04 18:20:52 -0400

    added code

---
+++ test.py
@@ -0,0 +1,44 @@
+ birth_year = input("enter your birth year: ")
+ age = 2022 - int(birth_year)
+ print(age)
+int()
+str()
+bool()
+float() 
+ first = input("first: ")
+ second = input("second: ")
+ sum  = first + second
+ print(sum)
+ course = 'python for beginners' #this is a object
+ print(course.upper())
+ print(course.find(y))
+ x = 10 x = x +3  x += 3 x = 10 + 3 * 2 # PEMDAS MATTERS
+ if stats temp = 19
+ if temp > 30 : print("its a hot day") print("drink plently of water") elif temp > 20 : print("its a nice day") else : print("done: nuff said")
+
+ while loops
+i = 0
+while i <= 5 :
+  print(i * '*')
+  i = i + 1
+# lists in py
+names = ['jon', "tyker", "yefim", "ethan"]
+print(names[0])
+names[0] = 'john'
+print(names[0:3])
+nums = [1,2,3,4,5]
+nums.remove(3)
+print(nums)
+# FOR LOOP
+for item in nums : 
+  print(item)
+  
+i = 0
+while  i < len(nums) : 
+  print(nums[i])
+  i= i + 1
+
+# ranges 
+numberss  = range(5)
+#turples are paraethesis immutable
+nu = (1, 2, 3)
